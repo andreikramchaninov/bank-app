3f6d6b8510b5468f761ba1170b27b9d98e3bc35d
andeikramchaninov <andreikramchaninov@gmail.com>
2017-09-12 20:41:11 +0300

    schema and data sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+create table if not exists customer (
+	id int(10) not null auto_increment,
+  fullname varchar(30) not null,
+  address varchar(75) not null,
+  age int(10) not null,
+  primary key (id)
+);
+
+create table if not exists bank_account (
+	id int(10) not null auto_increment,
+  customer_id int(10) not null,
+  money decimal(15, 4) not null,
+  primary key (id),
+  constraint foreign key (customer_id) references customer (id)
+);
+
+create table if not exists bank_operation (
+	id int (10) not null auto_increment,
+  account_id int(10) not null,
+  op_type tinyint(1) not null,
+  money decimal(15, 4) not null,
+  op_date timestamp not null,
+  primary key (id),
+  constraint foreign key (account_id) references bank_account (id)
+);
